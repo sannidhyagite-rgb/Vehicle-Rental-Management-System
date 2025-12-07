bad80d62d53e663f5d7570df5d4c953ae83bbe2e
patilatharv915 <patilatharv915@gmail.com>
2025-12-07 19:29:42 +0530

    change(app): vendor routes in App.jsx

--- drivenow-frontend/src/App.jsx
+++ drivenow-frontend/src/App.jsx
@@ -1,7 +1,6 @@
 // src/App.jsx
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import Navbar from "./components/layout/Navbar";
 import Hero from "./components/layout/Hero";
@@ -10,6 +9,15 @@ import FeaturedVehicles from "./components/home/FeaturedVehicles";
 
 import LoginPage from "./components/auth/LoginPage";
 import SignupPage from "./components/auth/SignupPage";
+
+/* Vendor pages */
+import VendorNavbar from "./vendor/layout/VendorNavbar";
+import VendorProfile from "./vendor/profile/VendorProfile";
+import VendorDashboard from "./vendor/dashboard/VendorDashboard";
+import MyVehicles from "./vendor/vehicles/MyVehicles";
+import AddVehicle from "./vendor/vehicles/AddVehicle";
+import VendorEarnings from "./vendor/earnings/VendorEarnings";
+import VendorNotifications from "./vendor/notifications/VendorNotifications";
 
 function Home() {
   return (
@@ -22,6 +30,16 @@ function Home() {
   );
 }
 
+function VendorShell({ element: Element }) {
+  // Wrapper to show vendor navbar and the page
+  return (
+    <>
+      <VendorNavbar />
+      <Element />
+    </>
+  );
+}
+
 function App() {
   return (
     <BrowserRouter>
@@ -29,6 +47,17 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
+
+        {/* vendor routes */}
+        <Route path="/vendor/profile" element={<VendorShell element={VendorProfile} />} />
+        <Route path="/vendor/dashboard" element={<VendorShell element={VendorDashboard} />} />
+        <Route path="/vendor/vehicles" element={<VendorShell element={MyVehicles} />} />
+        <Route path="/vendor/vehicles/new" element={<VendorShell element={AddVehicle} />} />
+        <Route path="/vendor/earnings" element={<VendorShell element={VendorEarnings} />} />
+        <Route path="/vendor/notifications" element={<VendorShell element={VendorNotifications} />} />
+
+        {/* catch-all */}
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
   );
