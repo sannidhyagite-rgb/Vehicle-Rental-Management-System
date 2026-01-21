9ed63e97941a462d3be5262d5371066cd7428c5d
sonbhaughode24 <ghodeshreyas@gmail.com>
2026-01-21 11:55:50 +0530

    Resolved App.jsx merge conflict

--- drivenow-frontend/src/App.jsx
+++ drivenow-frontend/src/App.jsx
@@ -1,7 +1,13 @@
 // src/App.jsx
 import React from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-//import { BrowserRouter, Routes, Route } from "react-router-dom";
+<<<<<<< HEAD
+=======
+
+import AdminDashboard from "./components/Admin/AdminDashboard/AdminDashboard";
+import AdminUser from "./components/Admin/AdminUser/AdminUser";
+import AdminNavbar from "./components/Admin/AdminNavbar/Navbar";
+>>>>>>> 2d482fe54230ff672c5b960e84ec746eb27c9fe2
 
 import Navbar from "./components/layout/Navbar";
 import Hero from "./components/layout/Hero";
@@ -12,21 +18,24 @@ import Footer from "./components/home/Footer";
 import LoginPage from "./components/auth/LoginPage";
 import SignupPage from "./components/auth/SignupPage";
 import MyBookings from "./components/MyBooking/MyBookings";
+import BookingInfo from "./pages/BookingInfo";
 
 import CustomerDashboard from "./pages/CustomerDashboard";
 import VendorRegister from "./pages/VendorRegister";
 import VehicleDetails from "./pages/VehicleDetails";
 import CNavbar from "./components/layout/CNavbar";
-//import VendorDashboard from "./pages/VendorDashboard";
+<<<<<<< HEAD
+=======
+>>>>>>> 2d482fe54230ff672c5b960e84ec746eb27c9fe2
 
-/* Vendor pages */
+// Vendor pages
 import VendorNavbar from "./vendor/layout/VendorNavbar";
 import VendorProfile from "./vendor/profile/VendorProfile";
-import VendorDashboard from "./vendor/dashboard/VendorDashboard";
 import MyVehicles from "./vendor/vehicles/MyVehicles";
 import AddVehicle from "./vendor/vehicles/AddVehicle";
 import VendorEarnings from "./vendor/earnings/VendorEarnings";
 import VendorNotifications from "./vendor/notifications/VendorNotifications";
+import VendorDashboard from "./vendor/dashboard/VendorDashboard"; // ← IMPORTANT!!
 
 function Home() {
   return (
@@ -41,8 +50,16 @@ function Home() {
   );
 }
 
+function AdminShell({ element: Element }) {
+  return (
+    <>
+      <AdminNavbar />
+      <Element />
+    </>
+  );
+}
+
 function VendorShell({ element: Element }) {
-  // Wrapper to show vendor navbar and the page
   return (
     <>
       <VendorNavbar />
@@ -55,78 +72,44 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* Public landing page */}
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignupPage />} />
 
-        {/* vendor routes */}
-        <Route path="/vendor/profile" element={<VendorShell element={VendorProfile} />} />
+        {/* Landing */}
+        <Route path="/" element={<Home />} />
+
+        {/* Admin */}
+        <Route path="/admin/dashboard" element={<AdminShell element={AdminDashboard} />} />
+        <Route path="/admin/users" element={<AdminShell element={AdminUser} />} />
+
+        {/* Vendor */}
         <Route path="/vendor/dashboard" element={<VendorShell element={VendorDashboard} />} />
+        <Route path="/vendor/profile" element={<VendorShell element={VendorProfile} />} />
         <Route path="/vendor/vehicles" element={<VendorShell element={MyVehicles} />} />
         <Route path="/vendor/vehicles/new" element={<VendorShell element={AddVehicle} />} />
         <Route path="/vendor/earnings" element={<VendorShell element={VendorEarnings} />} />
         <Route path="/vendor/notifications" element={<VendorShell element={VendorNotifications} />} />
 
-        {/* catch-all */}
+        {/* Auth */}
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+
+        {/* Customer */}
+        <Route path="/customerdashboard" element={<><CNavbar /><CustomerDashboard /></>} />
+
+        {/* Vendor Register */}
+        <Route path="/vendor/register" element={<><Navbar /><VendorRegister /></>} />
+
+        {/* Vehicle Details */}
+        <Route path="/vehicle/:id" element={<><CNavbar /><VehicleDetails /></>} />
+
+        {/* My Bookings */}
+        <Route path="/mybookings" element={<><CNavbar /><MyBookings /></>} />
+
+        {/* 404 */}
         <Route path="*" element={<Navigate to="/" replace />} />
+        <Route path="/booking-info" element={<BookingInfo />} />
+       <Route path="/payment" element={<Payment />} />
+       <Route path="/confirmation" element={<Confirmation />} />
 
-{/* Auth pages */}
-<Route path="/login" element={<LoginPage />} />
-<Route path="/signup" element={<SignupPage />} />
-
-{/* Customer Dashboard */}
-<Route
-  path="/customerdashboard"
-  element={
-    <>
-      <CNavbar />
-      <CustomerDashboard />
-    </>
-  }
-/>
-
-{/* Vendor Register Page */}
-<Route
-  path="/vendor/register"
-  element={
-    <>
-      <Navbar />
-      <VendorRegister />
-    </>
-  }
-/>
-
-{/* Vehicle Details Page */}
-<Route
-  path="/vehicle/:id"
-  element={
-    <>
-      <CNavbar />
-      <VehicleDetails />
-    </>
-  }
-/>
-
-{/* Vendor Dashboard */}
-<Route
-  path="/vendor/dashboard"
-  element={
-    <>
-      <CNavbar />
-      <VendorDashboard />
-    </>
-  }
-/>
-// My Bookings Page
-
-
-<Route path="/mybookings" element={
-  <>
-    <CNavbar />
-    <MyBookings />
-  </>
-} />
       </Routes>
     </BrowserRouter>
   );
